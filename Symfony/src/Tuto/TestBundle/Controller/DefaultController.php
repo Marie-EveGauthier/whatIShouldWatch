4f79e5fb12efa55f4abaae965f5bf66e7eed37ed
khaled <khaledtotti1074@hotmail.fr>
2016-12-29 16:47:48 -0600

    new new

---
+++ Symfony/src/Tuto/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tuto\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller {
+
+    public function testAction() {
+        $film = $this->getDoctrine()
+                ->getRepository('TutoTestBundle:films')
+                ->findall();
+
+        return $this->render('TutoTestBundle:Default:filmview.html.twig', array(
+                    'film' => $film));
+    }
+
+    public function secondAction() {
+
+        $director = $this->getDoctrine()
+                ->getRepository('TutoTestBundle:Directors')
+                ->findall();
+
+        return $this->render('TutoTestBundle:Default:directorview.html.twig', array(
+                    'director' => $director));
+    }
+
+    public function thirdAction() {
+
+        $writer = $this->getDoctrine()
+                ->getRepository('TutoTestBundle:writers')
+                ->findall();
+
+        return $this->render('TutoTestBundle:Default:writerview.html.twig', array(
+                    'writer' => $writer));
+    }
+
+    public function indexAction() {
+
+
+        return $this->render('TutoTestBundle:Default:index.html.twig');
+    }
+
+}
---
+++ Symfony/src/Tuto/TestBundle/TutoTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tuto\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TutoTestBundle extends Bundle
+{
+}
