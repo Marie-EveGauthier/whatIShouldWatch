2aee83b773d1a193d063ade2c4d9355b700a7f26
eileen_maeve <eileen.maeve@gmail.com>
2017-01-14 14:59:05 -0500

    made some cosmetic changes to the two scripts that get and process wiki categories

--- bin/process_categories.py
+++ bin/process_categories.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-filename1 = raw_input("Enter the name of the file you want to process categories from:")
+filename1 = raw_input("Enter the name of the file you want to process categories from:\n")
 
 #Import the data in file as a pandas object
 data = pd.read_csv(filename1, names = ['name', 'categories'])
@@ -10,7 +10,7 @@ data = pd.read_csv(filename1, names = ['name', 'categories'])
 no_categories = data[data.categories.isnull()]['name'].tolist()
 
 #and saves it to a file
-filename2 = raw_input("Enter a filename to save the list of names with no entries on wikipedia")
+filename2 = raw_input("Enter a filename to save the list of names with no entries on wikipedia:\n")
 with open(filename2, 'wb') as h:
     for name in no_categories:
         h.write(name +'\n')
@@ -18,7 +18,7 @@ with open(filename2, 'wb') as h:
 have_cats = data[data.categories.notnull()]
 women = have_cats[have_cats['categories'].str.contains("women")]['name'].tolist()
 
-filename3 = raw_input("Enter a filename to save the list of names of people who are women")
+filename3 = raw_input("Enter a filename to save the list of names of people who are women:\n")
 
 with open(filename3, 'wb') as h:
     for name in women:
