3dd60da5056314956fe72ba6f165782a407d585e
eileen_maeve <eileen.maeve@gmail.com>
2017-01-21 17:19:37 -0500

    Added script to save director gender data in one file

---
+++ bin/save_director_gender.py
@@ -0,0 +1,36 @@
+import csv
+
+
+women_directors = []
+
+with open('women_directors.txt','rb') as h:
+    for line in h:
+        line = line.lstrip().rstrip() #strip whitespace from beginning and newline from end
+        if line == "Robert Pulcini": continue
+        women_directors.append(line)
+
+women_writers = []
+with open('women_writers.txt', 'rb') as h:
+    for line in h:
+        line = line.lstrip().rstrip()
+        women_writers.append(line)
+
+with open('writerswomen.txt') as h:
+    for line in h:
+        line = line.lstrip().rstrip()
+        women_writers.append(line)
+
+allrows = []
+with open('directors_data.csv', 'rb') as csvfile:
+    with open('directors_data_gender.csv', 'wb') as output:
+        reader = csv.reader(csvfile)
+        writer = csv.writer(output)
+        for line in reader:
+            if line[0] in women_directors:
+                line[2]=True
+                #line.append("1")
+            else:
+                #line.append("0")
+                line[2]=False
+            allrows.append(line)
+        writer.writerows(allrows)
