3bbef75e9c6bb91981b6f5f9ce7db043e141700f
khaled <khaledtotti1074@hotmail.fr>
2017-01-25 15:05:10 -0600

    adding filter and delete views wand actions we dont need

--- Symfony/src/Tuto/TestBundle/Controller/DefaultController.php
+++ Symfony/src/Tuto/TestBundle/Controller/DefaultController.php
@@ -2,7 +2,13 @@
 
 namespace Tuto\TestBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\RedditAuthor;
+use AppBundle\Entity\RedditPost;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 
@@ -15,36 +21,52 @@ class DefaultController extends Controller {
                     'film' => $film));
     }
 
-    public function secondAction() {
-
-        $director = $this->getDoctrine()
-                ->getRepository('TutoTestBundle:Directors')
-                ->findall();
-
-        return $this->render('TutoTestBundle:Default:directorview.html.twig', array(
-                    'director' => $director));
-    }
-
-    public function thirdAction() {
-
-        $writer = $this->getDoctrine()
-                ->getRepository('TutoTestBundle:writers')
-                ->findall();
-
-        return $this->render('TutoTestBundle:Default:writerview.html.twig', array(
-                    'writer' => $writer));
-    }
-
-    public function indexAction() {
-
-
-        return $this->render('TutoTestBundle:Default:index.html.twig');
-    }
-
     public function filtersAction() {
 
         return $this->render('TutoTestBundle:Default:filters.html.twig');
-        
+    }
+
+    public function resultatAction(Request $request) {
+// for the request DBL
+        $em = $this->getDoctrine()->getManager();
+        // to get the value of the checkbox
+        $checked_woman = $this->get('request')->request->get('woman_name');
+        $checked_bechdel = $this->get('request')->request->get('button_bechdel');
+        if (($checked_woman) && ($checked_bechdel)) {
+            $query = $em->createQuery(
+                    'SELECT p
+    FROM TutoTestBundle:films p
+    WHERE p.dialogueWomen > p.dialogueMen and p.bechdel>0'
+            );
+
+            $products = $query->getResult();
+            return $this->render('TutoTestBundle:Default:resultat.html.twig', array(
+                        'products' => $products));
+        } else if ($checked_woman) {
+            $query = $em->createQuery(
+                    'SELECT p
+    FROM TutoTestBundle:films p
+    WHERE p.dialogueWomen > p.dialogueMen'
+            );
+
+            $products = $query->getResult();
+            return $this->render('TutoTestBundle:Default:resultat.html.twig', array(
+                        'products' => $products));
+        } else if ($checked_bechdel) {
+            $query = $em->createQuery(
+                    'SELECT p
+    FROM TutoTestBundle:films p
+    WHERE p.bechdel > 0'
+            );
+
+            $products = $query->getResult();
+            return $this->render('TutoTestBundle:Default:resultat.html.twig', array(
+                        'products' => $products));
+//..
+        } else {
+            return $this->render('TutoTestBundle:Default:filters.html.twig')
+            ;
+        }
     }
 
 }
